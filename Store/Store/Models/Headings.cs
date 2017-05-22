99c02158f2225c7726d4dab3b67b6ac471b71002
Черкас Руслан <brainttt@gmail.com>
2017-05-22 13:11:17 +0300

    Headings

--- Store/Store/Models/Headings.cs
+++ Store/Store/Models/Headings.cs
@@ -5,11 +5,9 @@ using System.Web;
 
 namespace Store.Models
 {
-    public class Order
+    public class Headings
     {
         public int Id { get; set; }
-        public DateTime Time { get; set; }
-        public string Status { get; set; }
-        public virtual User User { get; set; }
+
     }
 }
