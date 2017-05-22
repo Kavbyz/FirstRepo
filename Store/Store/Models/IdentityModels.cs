5940c1f79f06701c91b9fb4cb6b991ecc2b18545
Черкас Руслан <brainttt@gmail.com>
2017-05-22 14:13:07 +0300

    BaseReady

--- Store/Store/Models/IdentityModels.cs
+++ Store/Store/Models/IdentityModels.cs
@@ -10,6 +10,7 @@ namespace Store.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
