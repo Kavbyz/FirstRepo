bcaca3355687cc0634ee32c57f908f84ddf12abb
Черкас Руслан <brainttt@gmail.com>
2017-05-22 12:45:56 +0300

    Order

--- Store/Store/Models/User.cs
+++ Store/Store/Models/User.cs
@@ -10,7 +10,7 @@ namespace Store.Models
         public int Id { get; set; }
         public string FIO { get; set; }
         public string Email { get; set; }
-        public int Telephone { get; set; }       
-
+        public int Telephone { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
