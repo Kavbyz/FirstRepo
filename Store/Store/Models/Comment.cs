c5ee65168ca2d49c6195b1c3b26abb65db42cc10
skypeemail5g <skypeemail5g@gmail.com>
2017-05-22 13:12:13 +0300

    Comment

---
+++ Store/Store/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Comment_Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
