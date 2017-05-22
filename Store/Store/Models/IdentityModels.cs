8b00b12b07086d46391785508f1fd1755e7b88ab
Черкас Руслан <brainttt@gmail.com>
2017-05-22 14:01:11 +0300

    Data

--- Store/Store/Models/IdentityModels.cs
+++ Store/Store/Models/IdentityModels.cs
@@ -3,12 +3,18 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace Store.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FIO { get; set; }
+        public int Telephone { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +26,12 @@ namespace Store.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Headings> Headings { get; set; }
+        public DbSet<Images> Images { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
