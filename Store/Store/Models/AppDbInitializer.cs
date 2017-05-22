f6a96ef75a0dfc2bfdbe99e62455d1d9936512a3
Черкас Руслан <brainttt@gmail.com>
2017-05-22 14:05:19 +0300

    DbInitialize

---
+++ Store/Store/Models/AppDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Store.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+
+            base.Seed(context);
+        }
+    }
+}
