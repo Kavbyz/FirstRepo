cd39a03487f4fe6d144874b5afa8aefbae8bd406
Черкас Руслан <brainttt@gmail.com>
2017-05-22 13:51:58 +0300

    Images

--- Store/Store/Models/Product.cs
+++ Store/Store/Models/Product.cs
@@ -6,17 +6,21 @@ using System.Web;
 namespace Store.Models
 {
     public class Product
-    {
-        public Product()
-        {
-            Headings = new List<Headings>();
-        }
+    {        
         public int Id { get; set; }
         public string Name { get; set; }
         public int Count { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Headings> Headings { get; set; }
+        public virtual Order Order { get; set; }
+        public virtual ICollection<Images> Images { get; set; }
+
         //List image        
+
+        public Product()
+        {
+            Headings = new List<Headings>();
+        }
     }
 }
