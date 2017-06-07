987688f667c105733fa69e60d15eac52359e6d9f
Черкас Руслан <brainttt@gmail.com>
2017-06-07 17:33:15 +0300

    ЗалилКорзину

--- Store/Store/Controllers/BasketsController.cs
+++ Store/Store/Controllers/BasketsController.cs
@@ -252,10 +252,10 @@ namespace Store.Controllers
             db.SaveChanges();
             db.Count.Remove(count);
             db.SaveChanges();
-            if (basket.CountProduct.ToList().Count == 0)
-            {
-                return RedirectToAction("ViewBasket", new { idBasket = basket.Id });
-            }
+            //if (basket.CountProduct.ToList().Count == 0)
+            //{
+            //    return RedirectToAction("ViewBasket", new { idBasket = basket.Id });
+            //}
             return View("AddCount", basket.CountProduct.ToList());
         }
         //public async Task<ActionResult> Order()
